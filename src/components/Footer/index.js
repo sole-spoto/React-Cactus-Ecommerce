28d38b8349266344e81c7de26cf69fba1ccb195e
Soledad Spoto <sole.spoto@gmail.com>
2021-01-26 15:07:56 -0300

    react first project

---
+++ src/components/Footer/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+    FaFacebook,
+    FaInstagram,
+    FaTwitter,
+    FaYoutube
+} from 'react-icons/fa';
+import {
+    FooterContainer,
+    FooterWrap,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    SocialIcons,
+    SocialIconLink
+} from './FooterElements';
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to="/">Pizza</SocialLogo>
+                        <SocialIcons>
+                            <SocialIconLink href="/" target="_blanck" aria-label="Facebook" rel="noopener noreferrer">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blanck" aria-label="Instagram" rel="noopener noreferrer">
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blanck" aria-label="Twitter" rel="noopener noreferrer">
+                                <FaTwitter />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blanck" aria-label="Youtube" rel="noopener noreferrer">
+                                <FaYoutube />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer
